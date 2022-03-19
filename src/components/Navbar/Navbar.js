924041c7f14fbb82f73685922b315a36c1a62959
Agustin Perez <agustinjperez.r@gmail.com>
2022-03-19 18:32:23 -0300

    Menu E-commerce

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function NavBar(){
+    return <>
+        <nav className='nav'>
+            
+            <h2 className='titleNav'> soy una navbar </h2>
+            <div>
+            <button className='btnNav'>Nosotros</button>
+            <button className='btnNav'>Tienda</button>
+
+            </div>
+            
+        </nav>
+    </>
+}
+
+export default NavBar;
